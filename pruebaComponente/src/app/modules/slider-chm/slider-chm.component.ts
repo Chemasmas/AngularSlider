9678e53d1948a7730917f2daa652138c22a12327
Jorge Sierra <jorge.sierra.florido@gmail.com>
2018-03-28 17:40:22 -0600

    Primera prueba de concepto

--- pruebaComponente/src/app/modules/slider-chm/slider-chm.component.ts
+++ pruebaComponente/src/app/modules/slider-chm/slider-chm.component.ts
@@ -6,22 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider-chm.component.css']
 })
 export class SliderChmComponent implements OnInit {
-  
+
   imagenes = Array<String>();
-  catkey = "MjkwMjgz";
+  catkey = 'MjkwMjgz';
+  selected = 0;
 
 
   constructor() { }
 
   ngOnInit() {
     this.imagenes  = [
-      "https://source.unsplash.com/category/nature",
-      "https://source.unsplash.com/category/food",
-      "https://source.unsplash.com/category/buildings",
-      "https://source.unsplash.com/category/objects",
-      "https://source.unsplash.com/category/technology"
-    ]
+      'https://source.unsplash.com/category/nature',
+      'https://source.unsplash.com/category/food',
+      'https://source.unsplash.com/category/buildings',
+      'https://source.unsplash.com/category/objects',
+      'https://source.unsplash.com/category/technology'
+    ];
 
   }
+
+  cambiarImagen(i) {
+    this.selected = i;
+  }
+
 
 }
